d195dc7377435eb18e11def66ce24ae5bb45ffc7
Jalon Wong <jalonwong@gmail.com>
2026-09-18 10:12:55 -0500

    chore: bump version to v0.3.0

--- release/release.py
+++ release/release.py
@@ -1,16 +1,32 @@
+import argparse
 import platform
 import shutil
 
 from bump import get_info, run_cmd
 
-info = get_info()
-name = info["name"]
-version = info["version"]
-print(f"{name}: {version}", flush=True)
-
-run_cmd("pnpm tauri build")
-if platform.system() == "Linux":
-    run_cmd("makepkg --force --dir=release")
-    shutil.copy(
-        f"src-tauri/target/release/bundle/deb/{name}_{version}_amd64.deb", "release/"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--install",
+        action="store_true",
+        help="install PKGBUILD",
     )
+    opts = parser.parse_args()
+
+    info = get_info()
+
+    name = info["name"]
+    version = info["version"]
+    print(f"{name}: {version}", flush=True)
+
+    run_cmd("pnpm tauri build")
+    if platform.system() == "Linux":
+        cmd = "makepkg --force --dir=release"
+        if opts.install:
+            cmd += " -i"
+        run_cmd(cmd)
+        shutil.copy(
+            f"src-tauri/target/release/bundle/deb/{name}_{version}_amd64.deb",
+            "release/",
+        )
