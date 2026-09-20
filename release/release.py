4e90fc1043eeae4d1003d33b356d97b95d9e8f22
Jalon Wong <jalonwong@gmail.com>
2026-09-20 13:52:42 -0500

    Add GitHub workflow

--- release/release.py
+++ release/release.py
@@ -1,8 +1,23 @@
 import argparse
+import os
 import platform
 import shutil
+from pathlib import Path
 
 from bump import get_info, run_cmd
+
+
+def copy_file(name: str, src_dir: str, dist_dir: str) -> None:
+    src = Path(src_dir)
+    dist = Path(dist_dir)
+
+    for s in list(src.glob(name)):
+        d = dist.joinpath(os.path.basename(s))
+        if d.exists():
+            os.remove(d)
+        print(f"Copy: {s} -> {d}", flush=True)
+        shutil.copy(s, d)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -20,14 +35,33 @@ if __name__ == "__main__":
     version = info["version"]
     print(f"{name}: {version}", flush=True)
 
-    run_cmd("pnpm install")
+    os.chdir("src-tauri")
+    run_cmd("cargo build --release")
+    os.chdir("..")
     run_cmd("pnpm tauri build")
-    if platform.system() == "Linux":
+    if "cachyos" in platform.release():
         cmd = "makepkg --force --dir=release"
         if opts.install:
             cmd += " -i"
         run_cmd(cmd)
-        shutil.copy(
-            f"src-tauri/target/release/bundle/deb/{name}_{version}_amd64.deb",
-            "release/",
-        )
+
+    copy_file(
+        f"{name}*{version}*.deb",
+        "src-tauri/target/release/bundle/deb/",
+        "release/",
+    )
+    copy_file(
+        f"{name}*{version}*.msi",
+        "src-tauri/target/release/bundle/msi/",
+        "release/",
+    )
+    copy_file(
+        f"{name}*{version}*.dmg",
+        "src-tauri/target/release/bundle/dmg/",
+        "release/",
+    )
+    copy_file(
+        f"{name}*{version}*.rpm",
+        "src-tauri/target/release/bundle/rpm/",
+        "release/",
+    )
